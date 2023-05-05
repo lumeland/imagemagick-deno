b925dcea7f45dfc9d08acdfde8ddab5fe336ba78
Oscar Otero <oom@oscarotero.com>
2023-05-05 18:41:30 +0200

    new version with wasm cache

--- deno/mod.ts
+++ deno/mod.ts
@@ -19,6 +19,6 @@ export async function initialize() {
   }
 
   const response = await fetch(wasmUrl);
-  await cache.put(wasmUrl, response);
-  await initializeImageMagick(await response.clone().arrayBuffer());
+  await cache.put(wasmUrl, response.clone());
+  await initializeImageMagick(await response.arrayBuffer());
 }
