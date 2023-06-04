3bcac817a635bbd45c13466d14418167e9d8926f
Oscar Otero <oom@oscarotero.com>
2023-06-04 12:24:24 +0200

    don't cache if window.caches does not exist

--- deno/mod.ts
+++ deno/mod.ts
@@ -9,6 +9,12 @@ export async function initialize() {
     return;
   }
 
+  if (typeof caches === "undefined") {
+    const response = await fetch(wasmUrl);
+    await initializeImageMagick(await response.arrayBuffer());
+    return;
+  }
+
   const cache = await caches.open("magick_native");
   const cached = await cache.match(wasmUrl);
 
